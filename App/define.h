be3603450038510bf6054c0ba46f2248b8cace85
valerasan <yarovoy.valera2002@gmail.com>
2026-08-07 20:26:49 +0300

    add USB

--- App/define.h
+++ App/define.h
@@ -3,8 +3,13 @@
 
 #include "main.h"
 
+
+#define LOG(...)  do { printf(__VA_ARGS__); printf("\n"); } while(0)
+
 #define _LED_Blue_On        HAL_GPIO_WritePin(LED_Blue_GPIO_Port, LED_Blue_Pin, GPIO_PIN_SET);
 #define _LED_Blue_Off       HAL_GPIO_WritePin(LED_Blue_GPIO_Port, LED_Blue_Pin, GPIO_PIN_RESET);
 #define _LED_Blue_Toggle    HAL_GPIO_TogglePin(LED_Blue_GPIO_Port, LED_Blue_Pin);
+
+
 
 #endif // DEFINE__H
